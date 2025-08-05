872260ca3e6fd9678ff36eedf910e0aef228d160
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-08-05 22:09:50 +0300

    Create just.config.js

---
+++ just.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+ type: "redirect",
+ redirect_config: {
+    url: "https://justdeveloper.is-a.dev/",
+    params: {
+        title: "JustDeveloper",
+        description: "devcreates og",
+    }
+ }
+}
